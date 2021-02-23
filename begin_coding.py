7d62201976f6950e68d68612e46a1d405b559f5c
Adam Joines <amountcastlej@gmail.com>
2021-02-22 22:06:48 -0500

    Python file added

---
+++ begin_coding.py
@@ -0,0 +1,129 @@
+#print("Hello World!")
+
+#x = "Hello Python"
+#print(x)
+#y = 42
+#print(y)
+
+#print("this is a sample string")
+
+# ten = 10
+# one_hundred = 100
+# print(one_hundred-ten)
+
+#name = "Zen"
+#print("My name is ", name)
+
+#first_name = "Zen"
+#last_name = "Coder"
+#age = 27
+#print(f"My name is {first_name} {last_name} and I am {age} years old")
+
+#first_name = "Zen"
+#last_name = "Coder"
+#age = 27
+#print("My name is {} {} and I am {} years old".format(first_name, last_name, age))
+#print("My name is {} {} and I am {} years old".format(age, first_name, last_name))
+
+# name = "Adam"
+# stack = "Python"
+# print(f"The student {name} is the {stack} ")
+
+#hw = "Hello %s" % "world"
+#py = "I love Python %d" % 3
+#print(hw, py)
+
+#name = "Zen"
+#age = 27
+#print("My name is %s and I'm %d" % (name, age))
+
+#x = "hello world"
+#print(x.title())
+
+#string.upper()
+
+#string.lower()
+
+# hi = "hello there"
+# name = "Adam"
+# greet = hi + name
+# print(greet)
+
+# greeting = hi + " " + name
+# print(greeting)
+
+# silly = hi + (" " + name)*3
+# print(silly)
+
+# Lists - ordered
+
+# students = ['Vineet', 'Adam', 'Tim', 'Jon']
+# print(students[1])
+# print(len(students))
+# print(students[2])
+
+#Dictionaries
+
+# bird = {
+#     'feathers': "blue",
+#     'wings': 2,
+#     'name': "floppy"
+#     }
+# print(bird['wings'])
+#print(bird['friends'][1])
+
+#Conditionals
+# if bird['feathers']=='blue':
+#     print("it is a blue bird")
+# elif bird['feathers']=='red':
+#     print("It is a red bird")
+# else:
+#     print("it is not a red or blue bird")
+
+#Loops
+
+#for i in range(0, 100, 1):
+    # print(i)
+
+# for iterator in range(100):
+#     print(iterator)
+
+# for attribute in ninja:
+#     print(attribute)
+    # print(ninja[attribute])
+
+# bird = {
+#     'feathers': "blue",
+#     'wings': 2,
+#     'name': "floppy"
+#     }
+#for thing in bird:
+    # print(thing, bird[thing])
+
+# students = [
+#     {'first_name': 'Michael', 'last_name': 'Jordan'},
+#     {'first_name': 'John', 'last_name': 'Rosales'}
+# ]
+# print(students[0])
+# print(student[0]['first_name'])
+
+my_list = [1,2,3,4,5,6,7,8]
+
+for i in range(len(my_list)):
+    print(my_list[i])
+
+# Functions
+
+# def say_hello():
+#     print("Hello everyone!")
+#     say_hello()
+
+# def say_hello(name):
+#     print(f"Hello {name}")
+# for i in range(10):
+#     say_hello("James")
+
+# def say_hello():
+#     print(f"Hello everyone!")
+# for i in range(10):
+#     say_hello()
